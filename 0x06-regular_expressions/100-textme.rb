884b2e17209cfb0f74e88b4a8ac9783fcb17c7b7
Samuthe <ekatisamuel@gmail.com>
2022-08-23 17:24:46 +0300

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,4 +4,4 @@
 #The receiver phone number or name (including country code if present)
 #The flags that were used
 
-puts ARGV[0].scan(/from:(.)\].*to:(.)\].flags:(.)\].*\[msg/).join(",")
+puts ARGV[0].scan(/from:(.)\].*to:(.)\].flags:(.)\].*\[msg/).join(",")
