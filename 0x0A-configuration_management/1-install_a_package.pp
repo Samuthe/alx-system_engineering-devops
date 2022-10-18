8c918fdf0d5e916705a57401f5d589911df11eae
Samuthe <ekatisamuel@gmail.com>
2022-10-18 10:02:56 +0300

    kill

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,23 @@
 #Install flask
 #Version must be 2..0
 
+# Install a package using Puppet
+
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 3.0.0'
+}
+
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
+}
+
 package { 'puppet-lint':
-  ensure   => '3.0.0',
-  provider => 'gem',
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
 }
