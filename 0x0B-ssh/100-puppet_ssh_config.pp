820c7be51cd7871ba99ee852f738b37ab8ab4e9d
Samuthe <ekatisamuel@gmail.com>
2022-10-18 18:34:30 +0300

    add key

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#Client configuration file (w/ Puppet)
+
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
