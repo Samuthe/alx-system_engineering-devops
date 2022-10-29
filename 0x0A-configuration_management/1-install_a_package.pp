d9f7ce5a95d65240a9d9b1bed423b5c6875e4f56
Samuthe <ekatisamuel@gmail.com>
2022-10-29 15:01:57 +0300

    install puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,24 +1,6 @@
-#Using Puppet, install flask from pip3.
-#Install flask
-#Version must be 2..0
-
-# Install a package using Puppet
-
-exec { 'install ruby':
-  command => '/usr/bin/apt-get install -y ruby'
-}
-
-exec { 'install puppet-lint':
-  command => '/usr/bin/gem install puppet-lint -v 3.0.0'
-}
-
-package { 'ruby':
-  ensure => 'installed',
-  before => Exec['install ruby']
-}
+# Installs puppet-lint
 
 package { 'puppet-lint':
-  ensure  => 'installed',
-  before  => Exec['install puppet-lint'],
-  require => Package['ruby']
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
