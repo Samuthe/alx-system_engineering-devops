8b14461046c8d93efa16315507a5210453355f73
Samuthe <ekatisamuel@gmail.com>
2022-11-22 10:40:13 +0300

    mysql new user

--- 0x14-mysql/1-create_user_grant_permisions.sql
+++ 0x14-mysql/1-create_user_grant_permisions.sql
@@ -1,4 +1,4 @@
 CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost' WITH GRANT OPTIONS;
-FLUSH PRIVILEGES; # free memory
-SELECT user FROM mysql.user; # getting users on the mysql server
+GRANT REPLICATION SLAVE ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+SELECT user FROM mysql.user;
