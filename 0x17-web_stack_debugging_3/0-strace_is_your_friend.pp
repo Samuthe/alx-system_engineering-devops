a5aff06b10c65565044c25b09c6a9ed21aa07c69
Samuthe <ekatisamuel@gmail.com>
2022-11-29 16:41:20 +0300

    added web_dynamic folder

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fix setting in Wordpress
-exec { 'settingPress':
-  command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
-  provider => shell,
+# Creat a manifest that fix all termintion of phpp.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
